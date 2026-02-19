2fc04daec4646199e5d9e9eed314b7a0fc838082
acrr29 <alfz.29.rivr@gmail.com>
2026-02-19 16:11:14 +0000

    main menu

---
+++ Assets/Code/SceneLoader.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); //there is lowercarse & uppercase thing
+        }
+    }
+
+    public void LoadScene (string sceneName)
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReleoadScene()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    //to pause
+    public void TogglePause()
+    {
+        if(isPaused)
+        ResumeGame();
+        else 
+        PauseGame();
+    }
+
+    public void PauseGame()
+    {
+         Time.timeScale = 1f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+}
