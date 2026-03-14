5d3048af70c0170c9ec18ee2a9ae094b54b8cd59
acrr29 <alfz.29.rivr@gmail.com>
2026-03-14 16:52:00 +0000

    Added NPC and whatnot

--- Assets/Code/playerMovement.cs
+++ Assets/Code/playerMovement.cs
@@ -1,71 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 8f;
-    public float jumpForce = 12f;
-    public Transform groundCheck;
-    public float groundCheckDistance = 0.12f;
-    public Vector2 groundCheckOffset = new Vector2(0f, -0.5f);
+    public Rigidbody2D rb;
+    bool isFacingRight = true;
+    public ParticleSystem smokeFX;
+
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+
+    [Header("Dashing")]
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.1f;
+    public float dashCooldown = 0.1f;
+    bool isDashing;
+    bool canDash = true;
+    TrailRenderer trailRenderer;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    public int maxJumps = 2;
+    private int jumpsRemaining;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.49f, 0.03f);
     public LayerMask groundLayer;
 
-    private Rigidbody2D rb;
-    private float horizInput;
-    private bool isGrounded;
-    private SpriteRenderer spriteRenderer;
+    [Header("Gravity")]
+    public float baseGravity= 2f;
+    public float maxFallSpeed = 18f;
+    public float fallGravityMult = 2f;
 
-    private Animator animator;
-
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     void Update()
     {
-        horizInput = Input.GetAxisRaw("Horizontal");
-
-        Vector2 rayOrigin = groundCheck != null ? (Vector2)groundCheck.position : (Vector2)transform.position + groundCheckOffset;
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, groundCheckDistance, groundLayer);
-        isGrounded = hit.collider != null;
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if(isDashing)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.jumpSFX);
+            return;
         }
 
-        if (spriteRenderer != null)
-        {
-            if (horizInput > 0.1f) spriteRenderer.flipX = false;
-            else if (horizInput < -0.1f) spriteRenderer.flipX = true;
-        }
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
 
-        if(animator != null)
+        //falling gravity
+        if(rb.linearVelocity.y < 0)
         {
-            animator.SetFloat("moveInput", Mathf.Abs(horizInput));
-            animator.SetBool("isGrounded" , isGrounded);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizInput * speed, rb.linearVelocity.y);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * groundCheckDistance);
+            rb.gravityScale = baseGravity * fallGravityMult; //fall faster and faster
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed)); //max fall speed
         }
         else
         {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(transform.position + (Vector3)groundCheckOffset, transform.position + (Vector3)groundCheckOffset + Vector3.down * groundCheckDistance);
+            rb.gravityScale = baseGravity;
         }
+
+        GroundCheck();
+        Flip();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+  public void Dash(InputAction.CallbackContext context)
+    {
+        if(context.performed && canDash)
+        {
+            StartCoroutine(DashCoroutine());
+        }
+    }
+
+    private IEnumerator DashCoroutine()
+    {
+        canDash = false;
+        isDashing = true;
+
+        trailRenderer.emitting = true;
+        float dashDirection = isFacingRight ? 1f : -1f;
+
+        rb.linearVelocity = new Vector2(dashDirection * dashSpeed, rb.linearVelocity.y);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+
+        isDashing = false;
+        trailRenderer.emitting = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(jumpsRemaining > 0)
+        {
+            if (context.performed)
+            {
+                //Hold down jump button = full height
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+                jumpsRemaining--;
+                smokeFX.Play();
+            }
+            else if (context.canceled && rb.linearVelocity.y > 0)
+            {
+                //Light tap of jump button = half the height
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                jumpsRemaining--;
+                smokeFX.Play();
+            }
+        }
+    }
+
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) //checks if set box overlaps with ground
+        {
+            jumpsRemaining = maxJumps;
+        }
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontalMovement < 0f || !isFacingRight && horizontalMovement > 0f)
+        {
+            Vector3 ls = transform.localScale;
+            isFacingRight = !isFacingRight;
+            
+            ls.x *= -1f;
+            transform.localScale = ls;
+
+            if(rb.linearVelocity.y == 0)
+            {
+            smokeFX.Play();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Ground check visual
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
