fe72f13af50e49ba0cab03afb60d32095ae2c338
acrr29 <alfz.29.rivr@gmail.com>
2026-03-14 22:40:47 +0000

    inventory tab

---
+++ Assets/Code/InventoryController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int slotCount;
+    public GameObject[] itemPrefabs;
+
+    void Start()
+    {
+        for(int i = 0; i < slotCount; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if(i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
+    }
+
+    public bool AddItem(GameObject itemPrefab)
+    {
+        foreach(Transform slotTransform in inventoryPanel.transform)
+        {
+            Slot slot =slotTransform.GetComponent<Slot>();
+            if(slot != null && slot.currentItem == null)
+            {
+                GameObject newItem = Instantiate(itemPrefab, slotTransform);
+                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = newItem;
+                return true;
+            }
+        }
+
+        Debug.Log("Inventory is full!");
+        return false;
+    }
+}
